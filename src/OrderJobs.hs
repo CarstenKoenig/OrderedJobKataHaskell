1c2848b27e0c5e5d2cf807064482fa94ecf5a484
Carsten König <carsten@gettingsharper.de>
2017-07-14 08:19:32 +0200

    introduced a set-state to deal with the single dependency

--- src/OrderJobs.hs
+++ src/OrderJobs.hs
@@ -7,13 +7,44 @@ module OrderJobs
   , (.=>)
   ) where
 
-import Data.Char
+
+import qualified Control.Monad.Trans.State as St
+
+import           Data.Char
+
+import           Data.Set (Set)
+import qualified Data.Set as Set
 
 
 sort :: [Dependency] -> [Job]
-sort [] = []
-sort (Dependency job Nothing : rest) = job : sort rest
-sort (Dependency job (Just job') : rest) = job' : job : sort rest
+sort deps = St.evalState (sort' deps) Set.empty
+
+
+----------------------------------------------------------------------
+
+
+type Env a = St.State State a
+
+type State = Set String
+
+
+sort' :: [Dependency] -> Env [Job]
+sort' [] = return []
+sort' (Dependency job Nothing : rest) =
+  prependNew job $ sort' rest
+sort' (Dependency job (Just job') : rest) =
+  prependNew job' $ prependNew job $ sort' rest
+
+
+prependNew :: Job -> Env [Job] -> Env [Job]
+prependNew job cont = do
+  alreadyTaken <- St.gets (Set.member job)
+  if alreadyTaken
+    then cont
+    else do
+    St.modify (Set.insert job)
+    (job :) <$> cont
+  
 
 
 ----------------------------------------------------------------------
